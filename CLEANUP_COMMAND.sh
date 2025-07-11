6b483133b3525505ddfa8a432ea8c6653f5349dc
Alejandro Satlla <alejandrosatlla@MacBook-Pro-de-Alejandro.local>
2025-07-12 01:14:46 +0200

    Remove conflicting route directory

--- CLEANUP_COMMAND.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Execute this command to fix the build error:
-rm -rf app/api/public/resolve-property/\[propertyId\]
-rm -f URGENT_DELETE_DIRECTORY.md
-rm -f temp_delete_me.txt
-rm -f remove_conflicting_route.sh
-rm -f manual_cleanup.txt
-rm -f CLEANUP_COMMAND.sh
-echo "Build error fixed - conflicting route removed"
--- remove_conflicting_route.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf app/api/public/resolve-property/\[propertyId\]
-rm -f URGENT_DELETE_DIRECTORY.md
-rm -f temp_delete_me.txt
-echo "Conflicting route directory removed"
